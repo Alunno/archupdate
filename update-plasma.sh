9c9d5993b3968e27729224ce10746ff353ce761c
Edison Silva Filho <edisonsilvaf@gmail.com>
2025-06-26 21:52:06 -0300

    Novas alterações

--- update-plasma.sh
+++ update-plasma.sh
@@ -36,7 +36,7 @@ sleep 3
 yay -S --noconfirm --needed gwenview spectacle ark okular kcalc skanlite partitionmanager elisa krdc ktorrent kamoso kdegraphics-mobipocket kdegraphics-thumbnailers breeze-plymouth isoimagewriter gwenview dragon krita octopi bluedevil breeze-gtk drkonqi kde-gtk-config kdeplasma-addons kgamma kinfocenter krdp kscreen ksshaskpass kwallet-pam kwrited ocean-sound-theme oxygen oxygen-sounds plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-nm plasma-pa plasma-systemmonitor plasma-thunderbolt plasma-vault plasma-welcome plasma-workspace-wallpapers powerdevil print-manager sddm-kcm xdg-desktop-portal-kde kmail akonadi-import-wizard kmail-account-wizard kmailtransport mbox-importer kdenlive
 echo -e "${LYELLOW} ** Instalação de pacotes adicionais ** ${OFF}"
 sleep 3
-yay -S --noconfirm --needed conky-lua-nv lsb-release zsh testdisk p7zip zip unzip unrar aic94xx-firmware linux-firmware-qlogic wd719x-firmware ast-firmware lzo ffmpeg ffmpegthumbs ffmpegthumbnailer firefox firefox-i18n-pt-br libreoffice-fresh-pt-br aspell-pt hunspell-pt-br libreoffice-extension-vero libreoffice-impress-templates ttf-ms-fonts ttf-ubuntu-font-family ttf-roboto ttf-roboto-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-roboto ttf-roboto-mono ttf-font-logos ttf-font-awesome-5 nerd-fonts-git ttf-dejavu ttf-liberation gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gvfs gvfs-goa gvfs-google gvfs-smb vim yt-dlp youtube-dl dosfstools starship zsh-autocomplete-git zsh-syntax-highlighting-git zsh-autosuggestions-git smbclient sshfs ntfs-3g packagekit-qt6 epson-inkjet-printer-escpr cups ghostscript gsfonts gst-ffmpeg gstreamer visual-studio-code-bin kio-zeroconf kio-fuse kio-gdrive plymouth linux-headers gst-plugin-pipewire git btrfs-progs checkupdates-with-aur exfatprogs fwupd mtp mtpfs creality-print ttf-google-fonts obs-studio chitubox-free-bin
+yay -S --noconfirm --needed conky-lua-nv lsb-release zsh testdisk p7zip zip unzip unrar aic94xx-firmware linux-firmware-qlogic wd719x-firmware ast-firmware lzo ffmpeg ffmpegthumbs ffmpegthumbnailer firefox firefox-i18n-pt-br libreoffice-fresh-pt-br aspell-pt hunspell-pt-br libreoffice-extension-vero libreoffice-impress-templates ttf-ms-fonts ttf-ubuntu-font-family ttf-roboto ttf-roboto-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-roboto ttf-roboto-mono ttf-font-logos ttf-font-awesome-5 nerd-fonts-git ttf-dejavu ttf-liberation gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gvfs gvfs-goa gvfs-google gvfs-smb vim yt-dlp youtube-dl dosfstools starship zsh-autocomplete-git zsh-syntax-highlighting-git zsh-autosuggestions-git smbclient sshfs ntfs-3g packagekit-qt6 epson-inkjet-printer-escpr cups ghostscript gsfonts gst-ffmpeg gstreamer kio-zeroconf kio-fuse kio-gdrive plymouth linux-headers gst-plugin-pipewire git btrfs-progs checkupdates-with-aur exfatprogs fwupd mtp mtpfs creality-print ttf-google-fonts obs-studio
 echo -e "${YELLOW} ** Alterando o shell padrão para o zsh ** ${OFF}"
 sleep 3
 chsh -s /bin/zsh
@@ -58,10 +58,10 @@ echo -e "${RED} ** Removendo mais algumas coisas... ** ${OFF}"
 sleep 3
 cd /usr/share/applications/
 sudo rm avahi-discover.desktop bssh.desktop bvnc.desktop qv4l2.desktop qvidcap.desktop
-#echo -e "${LCYAN} ** Adicionando wallpapers... ** ${OFF}"
-#sleep 3
-#cd $LOCATION/KDE/
-#sudo cp -r Fusion_Goku /usr/share/wallpapers/ && sudo cp -r Goku_uv /usr/share/wallpapers/ && sudo cp -r Tribute /usr/share/wallpapers/
+echo -e "${LCYAN} ** Adicionando wallpapers... ** ${OFF}"
+sleep 3
+cd $LOCATION/KDE/
+sudo cp -r Campo /usr/share/wallpapers/ && sudo cp -r Lago /usr/share/wallpapers/ && sudo cp -r Chainise\ Wall /usr/share/wallpapers/
 cd /$HOME/archupdate
 echo -e "${LCYAN} ** Instalação de complementos bem sucedido... ** ${OFF}"
 sleep 3
